b091c3d99b4e47f02bf883ce95e87def5bb54df1
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-05-20 17:35:36 +0300

    commit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cnn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { connect } from "http2";
 
 export const BentoGrid = ({
   className,
@@ -81,14 +82,18 @@ export const BentoGridItem = ({
             <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
           </BackgroundGradientAnimation>
         )}
-      </div>
-
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          <div className="font-sans font-normal text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+            {description}
+          </div>
+          <div className="font-sans lg:text-3xl text-lg font-bold max-w-96 z-10">
+            {title}
+          </div>
         </div>
       </div>
     </div>
